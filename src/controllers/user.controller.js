3388975f78889e9877e72383d955ed3c8a15528e
shubham vishwakarma <shubhamon30@gmail.com>
2025-01-21 13:19:11 +0530

    session and cookies implementation

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,18 @@
+// Please don't change the pre-written code
+// Import the necessary modules here
+import { users } from "../models/user.model";
+import { registerUser } from "../models/user.model";
+export default class UserController {
+  getRegister = (req, res, next) => {
+    // Write your code here
+  };
+  getLogin = (req, res, next) => {
+    // Write your code here
+  };
+  addUser = (req, res) => {
+    // Write your code here
+  };
+  loginUser = (req, res) => {
+    // Write your code here
+  };
+}
